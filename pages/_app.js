edddc33f74777152130eadd9b93cb79de128a0cd
Marcelo Carvalho <marcelofilho09@hotmail.com>
2021-02-03 10:50:18 -0300

    favicon

--- pages/_app.js
+++ pages/_app.js
@@ -35,6 +35,9 @@ export default function App({ Component, pageProps }) {
             <Head>
                 <link rel="preconnect" href="https://fonts.gstatic.com" />
                 <link href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&display=swap" rel="stylesheet" />
+                <link rel="shortcut icon" href="/favicon.ico" />
+                <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+                <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
             </Head>
             <ThemeProvider theme={theme}>
                 <GlobalStyle />
